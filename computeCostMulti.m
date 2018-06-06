62b345f150f539a2b06ab9ebd5511e633267108b
Kyawmoehan <39848327+Kyawmoehan@users.noreply.github.com>
2018-06-06 19:17:30 +0630

    Update computeCostMulti.m

--- computeCostMulti.m
+++ computeCostMulti.m
@@ -1,4 +1,5 @@
 # Linear_regression_ml
+#computing cost for linear regression
 function J = computeCostMulti(X, y, theta)
 %COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
 %   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
